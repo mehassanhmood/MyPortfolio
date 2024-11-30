31cfc96c401ec56e8139164ad786143bdd77f922
Hassan Mehmood <mehassanhmood@gmail.com>
2024-11-30 04:12:43 -0500

    projects section fixed

--- src/components/utils/projects.ts
+++ src/components/utils/projects.ts
@@ -1,7 +1,7 @@
-// import inlfationImage from "/inflation_investments.png"
+
 export const projects = {
     "Data Visualization": [
-      { src: "/inflation_investments.png",  
+      { src: "./inflation_investments.png",  
         title: "Inflation and Investments",
         description: "Effects of inflation as a scrolly story",
       },
@@ -13,29 +13,29 @@ export const projects = {
     ],
     "Data Science": [
       {
-        src: "/neural_transfer.png",
+        src: "./neural_transfer.png",
         title: "Neural Style Transfer",
         description: "Starry Nights and City of Toronto",
       },
       {
-        src: "/Vaes&Gans.webp",
+        src: "./Vaes&Gans.webp",
         title: "Image Generation using VAEs and GANs",
         description: "Comparing GANs and VAEs built from scratch using the Celeb-A dataset, a widely used benchmark dataset for facial image generation.",
       },
       {
-        src: "/sentiment-analysis-trading.webp",
+        src: "./sentiment-analysis-trading.webp",
         title: "Sentiment Analysis for Portfolio Optimization",
         description: "The central aim of this project is to utilize sentiment analysis to understand and predict stock market movements. The project is structured around several key objectives, emphasizing the role of sentiment analysis as a foundational element.",
       },
     ],
     "Data Engineering": [
       {
-        src: "/DataFlow.png",
+        src: "./DataFlow.png",
         title: "ETL Pipeline ",
         description: " The project involves extracting data from various resources, transforming it, and loading it into different databases.",
       },
       {
-        src: "/Data-Engineering.webp",
+        src: "./Data-Engineering.webp",
         title: "Seamless Data Engineering: From Integration to Insights",
         description: "This project leverages a robust data ecosystem, integrating MySQL, MongoDB, PostgreSQL, Hadoop, and Spark to create seamless ETL pipelines using Apache Airflow. Business insights are derived through IBM Cognos and Tableau dashboards, offering real-time analytics and decision-making support.",
       },
@@ -43,7 +43,7 @@ export const projects = {
 
     "Data Analytics":[
       {
-        src: "/Crowd-Funding.svg",
+        src: "./Crowd-Funding.svg",
         title: "Crowdfunding Success Unveiled: Insights and Optimization",
         description: "This project delves into crowdfunding data to uncover trends in pledges, backers, and campaign outcomes. Key findings include the dominance of U.S.-based campaigns, the optimal duration of five months, and factors influencing success rates.",
       },
